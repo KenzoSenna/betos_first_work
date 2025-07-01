665be3ab4bf56c1febf7d7cdd74903025fb3bd08
Kenzo de Oliveira Senna <kenzoandblade@gmail.com>
2025-07-01 01:43:15 -0400

    estilização boba de terminal (enter para continuar)

--- utilidades_cod.py
+++ utilidades_cod.py
@@ -2,3 +2,9 @@ import os
 
 def limpa_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def continuar():
+    resposta_user = input("<<< Digite ENTER para continuar >>>\n")
+    if resposta_user == "":
+        return
+    return continuar()
